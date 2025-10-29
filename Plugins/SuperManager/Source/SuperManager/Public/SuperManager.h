1685b31265f07013c51d622782cdd7e336c7cac9
NeverWinter.Night <1317817472@qq.com>
2025-10-30 01:47:32 +0800

    Update SuperManager.h

--- Plugins/SuperManager/Source/SuperManager/Public/SuperManager.h
+++ Plugins/SuperManager/Source/SuperManager/Public/SuperManager.h
@@ -4,12 +4,23 @@
 
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
+#include "Framework/MultiBox/MultiBoxExtender.h"
+#include "AssetRegistry/AssetData.h"
 
+/**
+ * SuperManager module
+ */
 class FSuperManagerModule : public IModuleInterface
 {
 public:
-
 	/** IModuleInterface implementation */
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
+
+private:
+	/** 索引到 Content Browser 的 Asset View Extenders 数组，用于移除时使用 */
+	int32 AssetViewExtenderIndex = INDEX_NONE;
+
+	/** 在内容浏览器资产视图右键时返回一个 Extender（添加自定义菜单） */
+	TSharedRef<FExtender> OnExtendContentBrowserAssetSelectionMenu(const TArray<FAssetData>& SelectedAssets);
 };
